ccc732de93a4ee5c91f0fa4276d135bd82e3f531
AceofGrades <47581354+AceofGrades@users.noreply.github.com>
2019-11-04 15:43:02 +1100

    Added tests folder, testingsuite, and GameAssembly

---
+++ Assets/Tests/TestingSuite.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class TestingSuite
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void TestingSuiteSimplePasses()
+        {
+            // Use the Assert class to test conditions
+        }
+
+        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+        [UnityTest]
+        public IEnumerator TestingSuiteWithEnumeratorPasses()
+        {
+            // Use the Assert class to test conditions.
+            // Use yield to skip a frame.
+            yield return null;
+        }
+    }
+}
